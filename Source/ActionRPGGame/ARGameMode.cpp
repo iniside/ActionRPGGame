fa26827417ece051431e885b473b741a4551bae2
Lukasz "iniside" Baran <xeavien@gmail.com>
2017-07-01 17:50:45 +0200

    removing files

--- Source/ActionRPGGame/ARGameMode.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
-
-#include "ARGameMode.h"
-#include "ARCharacter.h"
-#include "UObject/ConstructorHelpers.h"
-
-AARGameMode::AARGameMode()
-{
-}
