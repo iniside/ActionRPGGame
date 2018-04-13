45b82764e7346929b424376d1d32bfa9b04bfa81
Lukasz 'iniside' Baran <xeavien@gmail.com>
2018-04-13 19:47:56 +0200

    added OrionAnimation module (it doesn't really do anything)

---
+++ Plugins/OrionAnimation/Source/OrionAnimation/OrionInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "OrionInterface.h"
+
+
+// Add default functionality here for any IOrionInterface functions that are not pure virtual.
