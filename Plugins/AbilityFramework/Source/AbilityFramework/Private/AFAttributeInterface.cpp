050286b4cb3f272fbf5eca30aeba553494d4c731
Lukasz 'iniside' Baran <xeavien@gmail.com>
2018-05-16 21:34:35 +0200

    missing files

---
+++ Plugins/AbilityFramework/Source/AbilityFramework/Private/AFAttributeInterface.cpp
@@ -0,0 +1,7 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AbilityFramework.h"
+#include "AFAttributeInterface.h"
+
+
+// Add default functionality here for any IAFAttributeInterface functions that are not pure virtual.
---
+++ Plugins/AbilityFramework/Source/AbilityFramework/Private/AFSimpleInterface.cpp
@@ -0,0 +1,7 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AbilityFramework.h"
+#include "AFSimpleInterface.h"
+
+
+// Add default functionality here for any IAFSimpleInterface functions that are not pure virtual.
